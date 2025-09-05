9e94f30bf6c07861fac45051f99016f99cde63c2
xubo <bworkid@qq.com>
2025-09-05 17:40:22 +0800

    feat: measure performace

--- Assets/0_Basic/02_mathematical-surfaces/FunctionLibrary.cs
+++ Assets/0_Basic/02_mathematical-surfaces/FunctionLibrary.cs
@@ -24,6 +24,18 @@ public static class FunctionLibrary {
 
     public static Function GetFunction(FunctionName name) => functions[(int)name];
 
+    public static FunctionName GetNextFunctionName(FunctionName name) => (int)name < functions.Length - 1 ? name + 1 : 0;
+
+    public static FunctionName GetRandomFunctionName() {
+        FunctionName choice = (FunctionName)Random.Range(0, functions.Length);
+        return choice;
+    }
+
+    public static FunctionName GetRandomFunctionNameOtherThan(FunctionName name) {
+        FunctionName choice = (FunctionName)Random.Range(0, functions.Length);
+        return choice == name ? 0 : choice;
+    }
+
     public static Vector3 Wave(float u, float v, float t) {
         Vector3 p;
         p.x = u;
@@ -68,9 +80,9 @@ public static class FunctionLibrary {
 
     public static Vector3 Torus(float u, float v, float t) {
         float r1 = 0.75f;
-        //r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
         float r2 = 0.25f;
-        //r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
 
         float s = r1 + r2 * Cos(PI * v);
         Vector3 p;
@@ -79,5 +91,10 @@ public static class FunctionLibrary {
         p.z = s * Cos(PI * u);
 
         return p;
+    }
+
+
+    public static Vector3 Morph(float u, float v, float t, Function from, Function to, float progress) {
+        return Vector3.LerpUnclamped(from(u, v, t), to(u, v, t), SmoothStep(0,1,progress));
     }
 }
