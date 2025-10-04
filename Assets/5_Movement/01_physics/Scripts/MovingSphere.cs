9046ee1f83f4df0fbe5e0e39d8dbefd495b2432f
xubo <bworkid@qq.com>
2025-10-04 14:41:52 +0800

    feat: movemnet-orbit-camera

--- Assets/5_Movement/01_physics/Scripts/MovingSphere.cs
+++ Assets/5_Movement/01_physics/Scripts/MovingSphere.cs
@@ -30,6 +30,9 @@ namespace physics {
         [SerializeField]
         LayerMask probeMask = -1, stairsMask = -1;
 
+        [SerializeField]
+        Transform playerInputSpace;
+
         Rigidbody body;
 
         Vector3 velocity, desiredVelocity;
@@ -66,8 +69,20 @@ namespace physics {
             playerInput.y = Input.GetAxis("Vertical");
             playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
-            desiredVelocity =
-                new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            if (playerInputSpace) {
+                Vector3 forward = playerInputSpace.forward;
+                forward.y = 0f;
+                forward.Normalize();
+                Vector3 right = playerInputSpace.right;
+                right.y = 0f;
+                right.Normalize();
+                desiredVelocity =
+                    (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+            } else {
+                desiredVelocity =
+                    new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            }
+
 
             desiredJump |= Input.GetButtonDown("Jump");
         }
